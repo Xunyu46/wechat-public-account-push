6dfb1f747c6c18c6b6eb56f716237edf4a5c399a
Xunyu46 <106477787+Xunyu46@users.noreply.github.com>
2024-05-09 22:17:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oGN_e6Q0TfALhPiKDJoY5z6sHcpA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'JRfICSM5p4ep7AbCVBpKbxsIyOMr4NsPcf1ahpic5xU',
+      useTemplateId: 'CO_VB87mJBEUxX7Rimv7E6yPT3UqhNjBOhwnZTtBQLs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-01',
       festivals: [
